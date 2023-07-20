c59acf844aea4eb7fb29de03fdb2f0801c32cef6
Joshua Akuna <akunajoshua@gmail.com>
2023-07-20 15:24:56 +0100

    0-bubble_sort.c file commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - the function that sorts an array of integers in
+ *	ascending order using the Bubble sort.
+ *
+ * @array: an array of integers.
+ *
+ * @size: size of the array.
+ *
+ * Returns: Nothing.
+ *
+ * Description: prints the array after each time yu swap 2 elements.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp, swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swapped)
+			break;
+	}
+}
