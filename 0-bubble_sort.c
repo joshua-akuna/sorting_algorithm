cfdde143dfdb572d07b7b92379c3170d7d115970
Joshua Akuna <akunajoshua@gmail.com>
2023-07-21 14:31:53 +0100

    0-bubble_sort.c : correction : syntax error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j, temp, swapped;
+	unsigned int i, j, temp;
 
 	if (array == NULL || size < 2)
 		return;
@@ -30,4 +30,6 @@ void bubble_sort(int *array, size_t size)
 				array[j + 1] = temp;
 				print_array(array, size);
 			}
+		}
+	}
 }
