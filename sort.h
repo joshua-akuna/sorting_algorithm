9816cd675d1d1f630bc5f7d8ca1799ff819f8b81
Joshua Akuna <akunajoshua@gmail.com>
2023-07-24 03:25:59 +0100

    sort.h updated

--- sort.h
+++ sort.h
@@ -13,17 +13,24 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-
-void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+
+void quick_sort_array(int *arr, size_t size, int low, int high);
+int partition(int *arr, size_t size, int low, int high);
+void swap(int *num1, int *num2);
 #endif /*_SORT_H_*/
