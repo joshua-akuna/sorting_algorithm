532fd4ba5b4045ffa2f45d761e391c4218ef2860
Joshua Akuna <akunajoshua@gmail.com>
2023-07-25 23:37:38 +0100

    106-bitonic_sort.c description updated

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -5,37 +5,37 @@ void bitonic_merge(int *array, int first, int size, int direction);
 
 /**
  * bitonic_sort - sorts the array of integers in ascending order.
- * array: the array of integers to sort.
+ * @array: the array of integers to sort.
  * @size: size of the array to sort.
  */
 void bitonic_sort(int *array, size_t size)
 {
-        int dir = 1;
+	int dir = 1;
 
-        if (array == NULL || size < 2)
-                return;
-
-        bitonic_sort_algo(array, 0, size, dir);
+	if (array == NULL || size < 2)
+		return;
+	bitonic_sort_algo(array, 0, size, dir);
 }
 
 /**
  * bitonic_sort_algo - sorts the array of integers in ascending order
  *	recursively by splitting it into sub arrays.
  * @array: the array of integers to sort.
+ * @first: the first index of the sub array.
  * @size: size of the array.
  * @direction: specify the sort order, UP (1) or DOWN (0);
  */
 void bitonic_sort_algo(int *array, int first, int size, int direction)
 {
-        int mid;
+	int mid;
 
-        if (size > 1)
-        {
-                mid = size / 2;
-                bitonic_sort_algo(array, first, mid, 1);
-                bitonic_sort_algo(array, first + mid, mid, 0);
-                bitonic_merge(array, first, size, direction);
-        }
+	if (size > 1)
+	{
+		mid = size / 2;
+		bitonic_sort_algo(array, first, mid, 1);
+		bitonic_sort_algo(array, first + mid, mid, 0);
+		bitonic_merge(array, first, size, direction);
+	}
 }
 
 /**
@@ -47,21 +47,21 @@ void bitonic_sort_algo(int *array, int first, int size, int direction)
  */
 void bitonic_merge(int *array, int first, int size, int direction)
 {
-        int mid = size / 2, i, temp;
+	int mid = size / 2, i, temp;
 
-        if (size > 1)
-        {
-                for (i = first; i < first + mid; i++)
-                {
-                        if ((direction == 1 && array[i] > array[i + mid])
-                                || (direction == 0 && array[i] < array[i + mid]))
-                        {
-                                temp = array[i];
-                                array[i] = array[i + mid];
-                                array[i + mid] = temp;
-                        }
-                }
-                bitonic_merge(array, first, mid, direction);
-                bitonic_merge(array, first + mid, mid, direction);
-        }
+	if (size > 1)
+	{
+		for (i = first; i < first + mid; i++)
+		{
+			if ((direction == 1 && array[i] > array[i + mid])
+				|| (direction == 0 && array[i] < array[i + mid]))
+			{
+				temp = array[i];
+				array[i] = array[i + mid];
+				array[i + mid] = temp;
+			}
+		}
+		bitonic_merge(array, first, mid, direction);
+		bitonic_merge(array, first + mid, mid, direction);
+	}
 }
